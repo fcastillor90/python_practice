31cd8bc6882bfff51ad61385965c1573bac93f25
Fernando Castillo <fer.castillorubilar@gmail.com>
2024-04-02 18:45:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#ejercicio1
+print("Hola MMundo")
+#ejercicio2
+x=5;
+print(x)
+#ejercicio3
+a=3;
+b=7;
+print(a+b)
+#ejercicio4
+a=10
+if a == 10:
+    print("Es 10")
+else:
+    print("No es 10")
+#ejercicio5
