40edfe970e811fe52660e766ebb7052befcbc5f9
Fernando Castillo <fer.castillorubilar@gmail.com>
2024-04-04 22:25:16 -0300

    12

---
+++ ALF1/alf12.py
@@ -0,0 +1,6 @@
+pan = 3.49
+descuento = 0.6
+ventaPanAñejo = float(input("Ingrese cantidad de barras de pan antiguas vendidas : "))
+print("Valor del pan : "+ str(pan))
+print("Descuento aplicado : "+ str(descuento))
+print("Valor a pagar : "+ str((ventaPanAñejo*pan)*descuento))
