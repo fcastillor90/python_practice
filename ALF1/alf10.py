221d27cb23e84c5a14c2d039aed8798d9236df99
Fernando Castillo <fer.castillorubilar@gmail.com>
2024-04-04 22:08:07 -0300

    555

---
+++ ALF1/alf10.py
@@ -0,0 +1,6 @@
+pesoPayaso = 112
+pesoMuneca = 75
+payaso = float(input("Ingrese numero de payasos vendidos : "))
+muñecas = float(input("Ingrese numero de muñecas vendidas : "))
+pesoTotal = (pesoPayaso*payaso)+(pesoMuneca*muñecas)
+print("El peso total es de : "+str(pesoTotal)+"g")
